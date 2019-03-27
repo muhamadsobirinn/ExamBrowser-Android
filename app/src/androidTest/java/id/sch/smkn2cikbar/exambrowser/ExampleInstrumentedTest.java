5aafdd6aefcd63c8a5a28d12ff69923569ebe0a8
muhamadsobirinn <48981472+muhamadsobirinn@users.noreply.github.com>
2019-03-27 15:13:46 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/id/sch/smkn2cikbar/exambrowser/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/sch/smkn2cikbar/exambrowser/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("id.sch.smkn2cikbar.exambrowser", appContext.getPackageName());
+        assertEquals("/cbtid.sch.smanegeri1gringsing.exambrowser", appContext.getPackageName());
     }
 }
